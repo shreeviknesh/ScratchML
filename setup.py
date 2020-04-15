a2a7febf6321642d06ab6f865047f524addd35f5
shreeviknesh <luckyboy1998@gmail.com>
2020-04-15 14:33:29 +0530

    [Modify] Changed setup.py configuration

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-import setuptools
+from setuptools import setup, find_packages
+import shutil
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+shutil.rmtree('dist')
+
+setup(
     name="scratchml", 
-    version="0.1",
+    version="0.3",
     author="Shreeviknesh Sankaran",
     author_email="shreeviknesh@gmail.com",
     description="A python package with implementations of Machine Learning algorithms from scratch.",
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shreeviknesh/ScratchML",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8", 
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires='>=3.6',
 )
 
-import shutil
 shutil.rmtree('scratchml.egg-info')
 shutil.rmtree('build')
