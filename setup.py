90d17416447b7c323a7590d681f32a25a396df39
shreeviknesh <luckyboy1998@gmail.com>
2020-05-21 18:32:05 +0530

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
-from setuptools import setup, find_packages
-import shutil
+""" This file is used to create the binaries of the ScratchML package """
+
 import os
+import shutil
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 if 'dist' in os.listdir():
     shutil.rmtree('dist')
 
 setup(
-    name="scratchml", 
+    name="scratchml",
     version="0.3.7",
     author="Shreeviknesh Sankaran",
     author_email="shreeviknesh@gmail.com",
     description="A python package with implementations of Machine Learning algorithms from scratch.",
     license="MIT",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/shreeviknesh/ScratchML",
     packages=find_packages(),
@@ -24,7 +26,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8", 
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
